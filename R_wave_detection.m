4a42dcacb58ac392cba991f6c597eb1191970095
Unknown <edronet@enseirb-matmeca.fr>
2018-05-14 09:15:31 +0200

    R values OK

--- R_wave_detection.m
+++ R_wave_detection.m
@@ -66,52 +66,33 @@ data = conv(delay,data); % delay = 5+16+2+10
 
 %% thresholding 
 
-tresh = 1e13;
+tresh = 3e13;
 p = (h5 > tresh);
 
-figure(1);
-plot(h5);
-hold all;
-plot(1e13*p);
-
-% max_h5 = max(h5);
-% mean5 = mean (h5 );
-% poss_reg =(h5 > mean5*max_h5)';
-%  
-% left = find(diff([0 poss_reg])==1);
-% right = find(diff([poss_reg 0])==-1);
-%  
-% left=left-(6+16);  % cancle delay because of LP and HP
-% right=right-(6+16);% cancle delay because of LP and HP
-%  
-% for i=1:length(left)
-%     [R_value(i) R_loc(i)] = max( x1(left(i):right(i)) );
-%     R_loc(i) = R_loc(i)-1+left(i); % add offset
-%  
-%     [Q_value(i) Q_loc(i)] = min( x1(left(i):R_loc(i)) );
-%     Q_loc(i) = Q_loc(i)-1+left(i); % add offset
-%  
-%     [S_value(i) S_loc(i)] = min( x1(left(i):right(i)) );
-%     S_loc(i) = S_loc(i)-1+left(i); % add offset
-%  
-% end
-%  
-% % there is no selective wave
-% Q_loc=Q_loc(find(Q_loc~=0));
-% R_loc=R_loc(find(R_loc~=0));
-% S_loc=S_loc(find(S_loc~=0));
-%  
-% figure
-% subplot(2,1,1)
-% title('ECG Signal with R points');
-% plot (t,x1/max(x1) , t(R_loc) ,R_value , 'r^', t(S_loc) ,S_value, '*',t(Q_loc) , Q_value, 'o');
-% legend('ECG','R','S','Q');
-% subplot(2,1,2)
-% plot (t,x1/max(x1) , t(R_loc) ,R_value , 'r^', t(S_loc) ,S_value, '*',t(Q_loc) , Q_value, 'o');
-% xlim([1 3])
+% figure(1);
+% plot(h5);
+% hold all;
+% plot(1e13*p);
+% 
 
 
 %% detection of maxima
+
+mult = h5.*p;
+ 
+mult1 = data(1:length(p)).*p;
+
+
+figure(2);
+plot(1e11*data);
+hold all;
+plot(mult);
+
+[pks, abscisse] = findpeaks(mult1);
+pks(pks < 0) = 0;
+
+
+R_value = pks(pks > 0); % value of R peak abcsissa 
 
 
 
